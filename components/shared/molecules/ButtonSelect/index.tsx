76a230f66aadaa489854f2f22b18109d1dc7bf9b
동동이 <drdragon777@naver.com>
2022-07-05 11:29:01 +0900

    soma-192/feat - add ButtonSelect molecule

---
+++ components/shared/molecules/ButtonSelect/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import classnames from 'classnames';
+import type { StyleProps } from 'types/props';
+
+import { Check } from '../../atoms/icon';
+import $ from './style.module.scss';
+
+type Props = {
+  label: string;
+} & StyleProps;
+
+export default function ButtonSelect({ className, style, label }: Props) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked((clicked) => !clicked);
+  };
+
+  return (
+    <button
+      type="button"
+      className={classnames($['btn-select'], className, {
+        [$.clicked]: isClicked,
+      })}
+      style={{ ...style }}
+      onClick={handleClick}
+    >
+      <Check className={$.icon} />
+      <span>{label}</span>
+    </button>
+  );
+}
