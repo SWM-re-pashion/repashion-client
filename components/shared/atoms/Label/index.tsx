7f05e0b0eabded2dbf49b85edbea912183f2295b
동동이 <drdragon777@naver.com>
2022-07-08 00:54:22 +0900

    soma-192/feat - add BorderBox, Description, Label, Title atoms

--- components/shared/atoms/Label/index.tsx
+++ components/shared/atoms/Label/index.tsx
@@ -1,16 +1,12 @@
 import classNames from 'classnames';
-import type { StyleProps } from 'types/props';
+import type { DefaultProps } from 'types/props';
 
 import $ from './style.module.scss';
 
-type Props = {
-  label: string;
-} & StyleProps;
-
-export default function Label({ className, style, label }: Props) {
+export default function Label({ className, style, children }: DefaultProps) {
   return (
     <span {...{ style }} className={classNames($.label, className)}>
-      {label}
+      {children}
     </span>
   );
 }
