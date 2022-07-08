02130d9784e36c3cf665c3b07c6ada6be660afa4
동동이 <drdragon777@naver.com>
2022-07-08 16:22:08 +0900

    soma-231/chore - add constants

---
+++ constants/colors.ts
@@ -0,0 +1,22 @@
+const colors = [
+  ['Black', '#000'],
+  ['White', '#000'],
+  ['Beige', '#000'],
+  ['Ivory', '#000'],
+  ['silver', '#000'],
+  ['Gray', '#000'],
+  ['Red', '#000'],
+  ['Pink', '#000'],
+  ['Orange', '#000'],
+  ['Yellow', '#000'],
+  ['Brown', '#000'],
+  ['Khaki', '#000'],
+  ['Green', '#000'],
+  ['Light Green', '#000'],
+  ['Mint', '#000'],
+  ['Blue', '#000'],
+  ['Navy', '#000'],
+  ['Purple', '#000'],
+];
+
+export { colors };
