f54c1284738d3e25bf6aed891906156b1bd23ac4
동동이 <drdragon777@naver.com>
2022-07-11 17:24:49 +0900

    soma-227/rafactor - refactoring ButtonSelect

--- components/shared/molecules/ButtonSelect/index.tsx
+++ components/shared/molecules/ButtonSelect/index.tsx
@@ -1,46 +1,43 @@
-import { useState } from 'react';
-
 import classnames from 'classnames';
-import type { DefaultProps } from 'types/props';
+import type { StyleProps } from 'types/props';
 
 import { Check } from '../../atoms/icon';
 import $ from './style.module.scss';
 
 type Props = {
+  label: string;
+  isSelected: boolean;
+  handleClick: (value: string) => void;
   color?: string;
-} & DefaultProps;
+} & StyleProps;
 
 export default function ButtonSelect({
   className,
   style,
-  children,
+  label,
+  isSelected,
+  handleClick,
   color,
 }: Props) {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked((clicked) => !clicked);
-  };
-
   return (
     <button
       type="button"
       className={classnames($['btn-select'], className, {
         [$.color]: color,
-        [$.clicked]: isClicked && !color,
-        [$['clicked-color']]: isClicked && color,
+        [$.clicked]: isSelected && !color,
+        [$['clicked-color']]: isSelected && color,
       })}
       style={{ ...style }}
-      onClick={handleClick}
+      onClick={() => handleClick(label)}
     >
       {color ? (
         <div className={$['color-box']} style={{ backgroundColor: color }}>
-          {isClicked && <Check className={$.icon} />}
+          {isSelected && <Check className={$.icon} />}
         </div>
       ) : (
         <Check className={$.icon} />
       )}
-      <span>{children}</span>
+      <span>{label}</span>
     </button>
   );
 }
