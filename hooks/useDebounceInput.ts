aa3b181bab6edd6ac8bffbea85e2fddc8cabe368
동동이 <drdragon777@naver.com>
2022-07-13 04:52:40 +0900

    soma-227/chore - modify eslint & hook name

--- hooks/useDebounceInput.ts
+++ hooks/useDebounceInput.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 
 import { debounce } from '../lib/debounce';
 
-export default function useThrottleInput<T>(setInput: (inputValue: T) => void) {
+export default function useDebounceInput<T>(setInput: (inputValue: T) => void) {
   return useMemo(
     () => debounce<T>((inputValue: T) => setInput(inputValue), 200),
     [],
