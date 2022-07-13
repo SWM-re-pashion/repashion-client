f4f91cb8b39d8cdf8bf6a1a9d2624eaa65103fd4
동동이 <drdragon777@naver.com>
2022-07-13 14:16:33 +0900

    soma-227/chore - add color & modify basic constans

--- constants/basic.ts
+++ constants/basic.ts
@@ -1,5 +1,5 @@
 const genders = ['여성', '남성'];
-const bodyForms = ['마름', '날씬', '보통', '통통'];
+const bodyForms = ['마름', '보통', '통통', '뚱뚱'];
 const topSizes = ['XS', 'S', 'M', 'L', 'XL', '2XL', '3XL'];
 const bottomSizes = [...Array.from({ length: 16 }, (_, i) => `${22 + i}`)];
 
