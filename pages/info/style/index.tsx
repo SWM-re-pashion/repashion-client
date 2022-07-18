292ba582089126e54718674fe672ae7469a612d8
동동이 <drdragon777@naver.com>
2022-07-18 18:20:03 +0900

    soma-238/feat - add SSR

--- pages/info/style/index.tsx
+++ pages/info/style/index.tsx
@@ -3,30 +3,42 @@
 import { useRouter } from 'next/router';
 
 import { ReactElement, useState, useCallback } from 'react';
+import { dehydrate, QueryClient, useQuery } from 'react-query';
 
 import ButtonFooter from '@atoms/ButtonFooter';
 import ImgBox from '@atoms/ImgBox';
 import InfoHeader from '@molecules/InfoHeader';
 import InfoPageNum from '@molecules/InfoPageNum';
 import Layout from '@templates/Layout';
+import { getStyleImg } from 'api/getStyleImg';
 import { NextPageWithLayout } from 'pages/_app';
 import { useInfoStore } from 'store/useInfoStore';
 
 import $ from './style.module.scss';
 
-const EXAMPLE_URL =
-  'https://images.unsplash.com/photo-1618588507085-c79565432917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fA%3D%3D&w=1000&q=80';
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery('styles', getStyleImg);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
+const skeletonImgBox = Array.from({ length: 20 });
 
 export const StyleInfo: NextPageWithLayout = () => {
   const state = useInfoStore((stat) => stat);
   const handleClick = useInfoStore(useCallback((stat) => stat.infoUpdate, []));
   const [errorMsg, setErrorMsg] = useState('');
   const router = useRouter();
-
-  const imgBoxMocks = Array.from({ length: 12 }, (_, i) => ({
-    id: i,
-    src: EXAMPLE_URL,
-  }));
+  const { isLoading, isError, data, error } = useQuery('styles', getStyleImg, {
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+  });
 
   const handleSubmit = () => {
     if (state.styles.length < 2) {
@@ -45,19 +57,23 @@ export const StyleInfo: NextPageWithLayout = () => {
         선호하는 이미지를 선택주세요.
         <br /> 2개 이상 선택 가능해요.
       </InfoHeader>
+
       <section className={$['style-info']}>
-        {imgBoxMocks.map(({ id, src }) => (
-          <ImgBox
-            id={id}
-            key={src + id}
-            src={src}
-            alt={`${id}번 이미지`}
-            isNeedClick
-            isSelected={state.styles.includes(id)}
-            handleClick={handleClick}
-          />
-        ))}
+        {!isLoading && data
+          ? data.styles.map(({ id, src, alt }) => (
+              <ImgBox
+                {...{ id, src, alt }}
+                key={src + id}
+                isNeedClick
+                isSelected={state.styles.includes(id)}
+                handleClick={handleClick}
+              />
+            ))
+          : skeletonImgBox.map((_, idx) => (
+              <ImgBox key={`skeleton-${idx}`} isLoading />
+            ))}
       </section>
+
       <ButtonFooter onClick={handleSubmit} msg={errorMsg}>
         다음
       </ButtonFooter>
