b2df8f562e5059cf5eb6e77a2dfe42b59e386002
동동이 <drdragon777@naver.com>
2022-07-21 03:54:51 +0900

    soma-277/feat - add shop detail page

---
+++ pages/shop/[id]/index.tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react';
+
+import { Arrow } from '@atoms/icon/Arrow';
+import ImgSlide from '@organisms/ImgSlide';
+import Layout from '@templates/Layout';
+
+const EXAMPLE_URL =
+  'https://images.unsplash.com/photo-1618588507085-c79565432917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fA%3D%3D&w=1000&q=80';
+
+const imgBoxMocks = Array.from({ length: 4 }, (_, i) => ({
+  id: i + 1,
+  src: EXAMPLE_URL,
+  alt: `img-mock-${i}`,
+  width: 360,
+  height: 360,
+}));
+
+function ShopDetail() {
+  return (
+    <section>
+      <ImgSlide imgList={imgBoxMocks} />
+    </section>
+  );
+}
+
+ShopDetail.getLayout = function getLayout(page: ReactElement) {
+  return <Layout noPadding>{page}</Layout>;
+};
+
+export default ShopDetail;
--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,11 @@ export type ActionProps = {
   type: string;
   payload: string;
 };
+
+export type ImgProps = {
+  id: number;
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+};
