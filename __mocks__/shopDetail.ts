413fddff9666ea665318eed3ebeafc1a7c0fadea
동동이 <drdragon777@naver.com>
2022-07-22 17:35:58 +0900

    soma-277/feat - add mock data

---
+++ __mocks__/shopDetail.ts
@@ -0,0 +1,54 @@
+export const shopDetail = {
+  isMe: true,
+  sellerInfo: {
+    profileImg:
+      'https://images.unsplash.com/photo-1618588507085-c79565432917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    nickname: '사용자128',
+  },
+  basic: {
+    title: '스투시 프린팅 티셔츠',
+    image: [
+      {
+        src: 'https://user-images.githubusercontent.com/62797441/180398288-76213583-0128-429e-bb1c-672a281e56dd.png',
+        alt: '이미지12',
+      },
+      {
+        src: 'https://user-images.githubusercontent.com/62797441/180398288-76213583-0128-429e-bb1c-672a281e56dd.png',
+        alt: '이미지23',
+      },
+      {
+        src: 'https://user-images.githubusercontent.com/62797441/180398288-76213583-0128-429e-bb1c-672a281e56dd.png',
+        alt: '이미지34',
+      },
+    ],
+    classification: '상의/탑',
+    brand: '스투시',
+    productInfo: '남성용/L',
+    styleInfo: '면/화이트/힙합',
+  },
+  sellerNotice: {
+    condition: '거의 없음',
+    pollution: '외관상 안 보임',
+    height: '170',
+    length: '엉덩이',
+    bodyForm: '보통',
+    fit: '루즈',
+    purchaseTime: '작년 9월',
+    purchasePlace: '스투시 온라인 매장',
+    reason:
+      '안 입는데 옷장에 있어서 팔아요 연락주시면 감사하겠습니다. 좋은 하루 되세요!',
+    opinion:
+      '상태도 좋고 세탁도 딱 한 번 했어요! 직거래 택배거래 모두 가능하니 편하게 연락주세요! 감사합니다.',
+  },
+  measure: {
+    length: 73,
+    shoulderWidth: 53,
+    chestSection: 59,
+    sleeveLength: 100,
+  },
+  price: 20000,
+  isIncludeDelivery: true,
+  createdAt: '2022-07-22 16:48', // yyyy-mm-dd hh:mm
+  like: 27,
+  views: 15,
+};
