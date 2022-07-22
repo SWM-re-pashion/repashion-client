72137a12cd8563d947b9b61aa0fa0cd3a8245bc5
동동이 <drdragon777@naver.com>
2022-07-22 17:36:24 +0900

    soma-277/feat - add ProductCell atom

---
+++ components/Product/atoms/ProductCell/index.tsx
@@ -0,0 +1,30 @@
+import { StyleProps } from '#types/props';
+import classnames from 'classnames';
+
+import $ from './style.module.scss';
+
+type Props = {
+  label: string;
+  description: string;
+  isBottom?: boolean;
+} & StyleProps;
+
+export default function ProductCell({
+  label,
+  description,
+  isBottom,
+  className,
+}: Props) {
+  return (
+    <div
+      className={classnames(
+        $['product-cell'],
+        { [$.bottom]: isBottom },
+        className,
+      )}
+    >
+      <strong className={$.label}>{label}</strong>
+      <span className={classnames($.label, $.description)}>{description}</span>
+    </div>
+  );
+}
