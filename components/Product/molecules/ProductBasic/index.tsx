99a073f636c6579ab0d77084bd687a7c6da31462
동동이 <drdragon777@naver.com>
2022-07-22 17:42:58 +0900

    soma-277/feat - add ProductBasic molecule

---
+++ components/Product/molecules/ProductBasic/index.tsx
@@ -0,0 +1,46 @@
+import { StyleProps } from '#types/props';
+import { replace } from 'utils/replace';
+
+import ProductCell from '../../atoms/ProductCell';
+import $ from './style.module.scss';
+
+type Props = {
+  basic: {
+    title: string;
+    classification: string;
+    brand: string;
+    productInfo: string;
+    styleInfo: string;
+  };
+} & StyleProps;
+
+export default function ProductBasic({ basic }: Props) {
+  const { title, classification, brand, productInfo, styleInfo } = basic;
+
+  const datas = [
+    { label: '브랜드', description: brand },
+    { label: '판매제품 정보', description: replace(productInfo, '/', '・') },
+    {
+      label: '스타일 정보',
+      description: replace(styleInfo, '/', '・'),
+      isBottom: true,
+    },
+  ];
+
+  return (
+    <>
+      <header className={$['product-header']}>
+        <h1 className={$['product-title']}>{title}</h1>
+        <span className={$['product-category']}>
+          {replace(classification, '/', ' > ')}
+        </span>
+      </header>
+
+      <article>
+        {datas.map((data) => (
+          <ProductCell key={data.label} {...data} />
+        ))}
+      </article>
+    </>
+  );
+}
