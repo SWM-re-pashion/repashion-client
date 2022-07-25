5770d985fec75f15fcabf3d804541bdb50c17e94
동동이 <drdragon777@naver.com>
2022-07-25 15:07:32 +0900

    soma-277/feat - add IconText atom

---
+++ components/shared/atoms/IconText/index.tsx
@@ -0,0 +1,29 @@
+import Button from '@atoms/Button';
+import classnames from 'classnames';
+import type { DefaultProps } from 'types/props';
+
+import $ from './style.module.scss';
+
+type Props = {
+  Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  color?: string;
+  colorText?: string;
+} & DefaultProps;
+
+export default function IconText({
+  className,
+  children,
+  Icon,
+  color,
+  colorText,
+}: Props) {
+  return (
+    <div className={classnames($['icon-text'], className)}>
+      <Icon className={$.icon} />
+      <span className={$.text} style={{ color }}>
+        {colorText}
+      </span>
+      <span className={$.text}>{children}</span>
+    </div>
+  );
+}
