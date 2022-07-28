4c458e207f9c94e7a53f54488629ececab5dbd4a
동동이 <drdragon777@naver.com>
2022-07-28 15:52:01 +0900

    soma-259/refactor - rafatoring FilterHeader

--- components/Shop/molecules/FilterHeader/index.tsx
+++ components/Shop/molecules/FilterHeader/index.tsx
@@ -9,15 +9,17 @@ type Props = {
 export default function FilterHeader({ onClose }: Props) {
   return (
     <header className={$['filter-header']}>
-      <button
-        type="button"
-        onClick={onClose}
-        className={$['filter-close']}
-        aria-label="필터 닫기"
-      >
-        <Close />
-      </button>
-      <h1 className={$.title}>필터</h1>
+      <div className={$['filter-header-box']}>
+        <button
+          type="button"
+          onClick={onClose}
+          className={$['filter-close']}
+          aria-label="필터 닫기"
+        >
+          <Close />
+        </button>
+        <h1 className={$.title}>필터</h1>
+      </div>
     </header>
   );
 }
