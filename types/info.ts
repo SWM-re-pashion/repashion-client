7851fa753f9d18cbec778d406078f2dd641194a5
동동이 <drdragon777@naver.com>
2022-07-28 10:49:11 +0900

    soma-277/feat - add FilterInfo type

--- types/info.ts
+++ types/info.ts
@@ -21,6 +21,19 @@ export type ColorUserInfo = {
   bottomColors: string[];
 };
 
+export type ClothesCategory = {
+  top: string[];
+  bottom: string[];
+};
+
+export type FilterInfo = {
+  styles: string[];
+  colors: ClothesCategory;
+  fit: ClothesCategory;
+  length: ClothesCategory;
+  size: ClothesCategory;
+};
+
 export type ColorData = { name: string; code: string };
 
 type btnBox = {
@@ -35,5 +48,12 @@ export type basicBtnBox = {
 
 export type colorBtnBox = {
   type: keyof ColorUserInfo;
+  isColor?: boolean;
+} & btnBox;
+
+export type filterBtnBox = {
+  type: keyof FilterInfo;
+  subType?: keyof ClothesCategory;
+  datas: string[] | ColorData[];
   isColor?: boolean;
 } & btnBox;
