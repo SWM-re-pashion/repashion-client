0e961cce65861fc584c85d7d976b547e1949a41d
동동이 <drdragon777@naver.com>
2022-07-29 14:39:25 +0900

    soma-259/feat - add delay parameter

--- hooks/useDebounceInput.ts
+++ hooks/useDebounceInput.ts
@@ -2,9 +2,12 @@ import { useMemo } from 'react';
 
 import { debounce } from '../lib/debounce';
 
-export default function useDebounceInput<T>(setInput: (inputValue: T) => void) {
+export default function useDebounceInput<T>(
+  setInput: (inputValue: T) => void,
+  delay: number,
+) {
   return useMemo(
-    () => debounce<T>((inputValue: T) => setInput(inputValue), 200),
+    () => debounce<T>((inputValue: T) => setInput(inputValue), delay),
     [],
   );
 }
