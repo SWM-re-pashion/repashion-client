675674328020fbf2afebc10cec94ed3fc24f6533
동동이 <drdragon777@naver.com>
2022-08-02 01:52:11 +0900

    soma-259/feat - add zustand persist

--- store/useFilterStore.ts
+++ store/useFilterStore.ts
@@ -2,90 +2,98 @@ import { FilterStoreState } from '#types/storeType/filter';
 import { FilterInfo, ClothesCategory } from 'types/info';
 import { updateInfo, deepClone } from 'utils';
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { filterCommonState, filterInitialState } from './constants';
 
-export const useFilterStore = create<FilterStoreState>((set) => ({
-  ...filterInitialState,
-  priceUpdate: (value: number, idx: number) => {
-    set((state) => {
-      const toBeModified: [number, number] = [...state.price];
-      toBeModified[idx] = value;
-      return { ...state, price: toBeModified };
-    });
-  },
+export const useFilterStore = create(
+  persist<FilterStoreState>(
+    (set) => ({
+      ...filterInitialState,
+      priceUpdate: (value: number, idx: number) => {
+        set((state) => {
+          const toBeModified: [number, number] = [...state.price];
+          toBeModified[idx] = value;
+          return { ...state, price: toBeModified };
+        });
+      },
 
-  clear: (subType: string) => {
-    const remain = subType === 'bottom' ? 'top' : 'bottom';
-    const current = subType === 'outer' ? 'top' : subType;
+      clear: (subType: string) => {
+        const remain = subType === 'bottom' ? 'top' : 'bottom';
+        const current = subType === 'outer' ? 'top' : subType;
 
-    set((state) => {
-      if (subType === 'all')
-        return {
-          ...state,
-          ...deepClone(filterCommonState),
-        };
-      return {
-        ...deepClone(filterCommonState),
-        colors: {
-          [remain]: [...state.colors[remain]],
-          [current]: [],
-        },
-        fit: {
-          [remain]: [...state.fit[remain]],
-          [current]: [],
-        },
-        length: {
-          [remain]: [...state.length[remain]],
-          [current]: [],
-        },
-        size: {
-          [remain]: [...state.size[remain]],
-          [current]: [],
-        },
-      };
-    });
-  },
-
-  filterUpdate: (
-    type: keyof FilterInfo,
-    value: string,
-    subType?: keyof ClothesCategory,
-  ) => {
-    set((state) => {
-      const removeAllCatecory = (arr: string[]) =>
-        arr.filter((x) => x !== '전체');
-      const isValueAll = typeof value === 'string' && value === '전체';
-
-      if (type === 'styles') {
-        if (type === 'styles' && isValueAll)
-          return { ...state, [type]: [value] };
-        return {
-          ...state,
-          [type]: removeAllCatecory(updateInfo<string>(state[type], value)),
-        };
-      }
-
-      if (subType) {
-        if (isValueAll)
+        set((state) => {
+          if (subType === 'all')
+            return {
+              ...state,
+              ...deepClone(filterCommonState),
+            };
           return {
-            ...state,
-            [type]: {
-              ...state[type],
-              [subType]: [value],
+            ...deepClone(filterCommonState),
+            colors: {
+              [remain]: [...state.colors[remain]],
+              [current]: [],
+            },
+            fit: {
+              [remain]: [...state.fit[remain]],
+              [current]: [],
+            },
+            length: {
+              [remain]: [...state.length[remain]],
+              [current]: [],
+            },
+            size: {
+              [remain]: [...state.size[remain]],
+              [current]: [],
             },
           };
-        return {
-          ...state,
-          [type]: {
-            ...state[type],
-            [subType]: removeAllCatecory(
-              updateInfo<string>(state[type][subType], value),
-            ),
-          },
-        };
-      }
-      return state;
-    });
-  },
-}));
+        });
+      },
+
+      filterUpdate: (
+        type: keyof FilterInfo,
+        value: string,
+        subType?: keyof ClothesCategory,
+      ) => {
+        set((state) => {
+          const removeAllCatecory = (arr: string[]) =>
+            arr.filter((x) => x !== '전체');
+          const isValueAll = typeof value === 'string' && value === '전체';
+
+          if (type === 'styles') {
+            if (type === 'styles' && isValueAll)
+              return { ...state, [type]: [value] };
+            return {
+              ...state,
+              [type]: removeAllCatecory(updateInfo<string>(state[type], value)),
+            };
+          }
+
+          if (subType) {
+            if (isValueAll)
+              return {
+                ...state,
+                [type]: {
+                  ...state[type],
+                  [subType]: [value],
+                },
+              };
+            return {
+              ...state,
+              [type]: {
+                ...state[type],
+                [subType]: removeAllCatecory(
+                  updateInfo<string>(state[type][subType], value),
+                ),
+              },
+            };
+          }
+          return state;
+        });
+      },
+    }),
+    {
+      name: 'filter-storage',
+    },
+  ),
+);
