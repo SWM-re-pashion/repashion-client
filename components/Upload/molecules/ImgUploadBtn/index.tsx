7e4f868374cb5d31cf0afad50d7f1df9ab3abe9a
동동이 <drdragon777@naver.com>
2022-08-04 16:16:27 +0900

    soma-279/feat - add ImgUploadBtn atom

---
+++ components/Upload/molecules/ImgUploadBtn/index.tsx
@@ -0,0 +1,42 @@
+import Button from '@atoms/Button';
+import { Upload } from '@atoms/icon';
+import InputFile from '@atoms/InputFile';
+import InputLabel from '@atoms/InputLabel';
+
+import $ from './style.module.scss';
+
+type Props = {
+  inputRef: React.RefObject<HTMLInputElement>;
+  num: number;
+  onUploadClick: () => void;
+  onUpload: (e: React.ChangeEvent) => void;
+};
+
+function ImgUploadBtn(imgProps: Props) {
+  const { inputRef, num, onUploadClick, onUpload } = imgProps;
+
+  return (
+    <Button
+      iconBtn
+      className={$['img-upload']}
+      onClick={onUploadClick}
+      aria-label="이미지 업로드 버튼"
+    >
+      <Upload />
+      <span className={$['img-num']}>
+        (<span className={$.num}>{num}</span>
+        /10)
+      </span>
+      <InputLabel htmlFor="chooseFile" text="이미지 업로드" />
+      <InputFile
+        ref={inputRef}
+        id="chooseFile"
+        accept=".jpg,.jpeg,.png,.webp,.heif,.heic,.bmp"
+        onChange={onUpload}
+        none
+      />
+    </Button>
+  );
+}
+
+export default ImgUploadBtn;
