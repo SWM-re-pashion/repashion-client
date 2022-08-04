a8800e82f534cf1e3bb24730fcecd2c8ec1dd6d7
동동이 <drdragon777@naver.com>
2022-08-05 01:32:56 +0900

    soma-279/feat - add ImgUpload organism

--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -1,15 +1,20 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useCallback } from 'react';
 
 import BackBtn from '@atoms/BackBtn';
 import PageHeader from '@molecules/PageHeader';
 import Layout from '@templates/Layout';
 import ImgUpload from 'components/Upload/organisms/ImgUpload';
 import StyleSelect from 'components/Upload/organisms/StyleSelect';
+import { useUploadStore } from 'store/useUploadStore';
 
 import { styleData } from './constants';
 import $ from './style.module.scss';
 
 function Uplaod() {
+  const imgUpload = useUploadStore(useCallback((state) => state.imgUpload, []));
+  const removeImg = useUploadStore(useCallback((state) => state.removeImg, []));
+  const states = useUploadStore((state) => state.imgList);
+
   return (
     <>
       <PageHeader
@@ -17,7 +22,7 @@ function Uplaod() {
         left={<BackBtn color="#000" className={$.back} />}
       />
       <div className={$.upload}>
-        <ImgUpload />
+        <ImgUpload dispatch={imgUpload} data={states} remove={removeImg} />
         <StyleSelect data={styleData} />
       </div>
     </>
--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,14 @@
+export type ImgBasicProps = {
+  id?: number;
+  src: string;
+};
+
 export type ImgProps = {
   id?: number;
   src: string;
-  alt: string;
+  alt?: string;
   width?: number;
   height?: number;
-};
+} & ImgBasicProps;
 
 export type DefaultData = { name: string; code: string };
