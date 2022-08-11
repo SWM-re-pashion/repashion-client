4da56e19d04c94edfaef696c3843a70873b8959a
동동이 <drdragon777@naver.com>
2022-08-11 15:15:23 +0900

    soma-279/feat - add Dialog util

--- components/Upload/organisms/Dialog/utils.ts
+++ components/Upload/organisms/Dialog/utils.ts
@@ -1,28 +1,23 @@
 import { deepClone, mergeObjInArr } from 'utils';
 
-export const findGender = (
+export const findChildren = (
   category: data.CategoryTree['children'],
-  name: string,
-) => category.find((gender) => gender.name === name);
-
-export const filteredCategory = (
-  category: data.CategoryTree,
-  name: string,
   code: string,
-) => {
-  const genderData = findGender(category.children, name);
-  const commonData = findGender(category.children, '공용');
+) => category.find((children) => children.code === code)?.children;
+
+export const filteredCategory = (code: string, category: data.CategoryTree) => {
+  const commonData = findChildren(category.children, 'common');
+  if (code === 'common') return commonData;
+  const genderData = findChildren(category.children, code);
   const mergedCategory = deepClone(
-    mergeObjInArr(
-      genderData?.children,
-      commonData?.children,
-      'name',
-      'children',
-    ),
+    mergeObjInArr(commonData, genderData, 'name', 'children'),
   );
-  return {
-    name,
-    code,
-    children: mergedCategory,
-  };
+  return mergedCategory;
+};
+
+export const categoryProperty = (idx: number) => {
+  if (idx === 0) return { name: '성별', code: 'gender' };
+  if (idx === 1) return { name: '메인 카테고리', code: 'mainCategory' };
+  if (idx === 2) return { name: '서브 카테고리', code: 'subCategory' };
+  return { name: '', code: '' };
 };
