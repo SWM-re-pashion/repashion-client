dea7223ec70887b537eee58236ad3c98cb15d059
동동이 <drdragon777@naver.com>
2022-08-18 12:13:44 +0900

    soma-279/feat - add measure organism

---
+++ components/Upload/organisms/Measure/index.tsx
@@ -0,0 +1,52 @@
+import { memo, useCallback } from 'react';
+
+import { DefaultData } from '#types/index';
+import { UpdateUpload, UploadState } from '#types/storeType/upload';
+import TextInput from '@atoms/TextInput';
+import InfoArticle from '@molecules/InfoArticle';
+import useDebounceInput from 'hooks/useDebounceInput';
+import { filterHeight } from 'utils/filterValue';
+
+import $ from './style.module.scss';
+
+type Props = {
+  data: DefaultData[];
+  state: UploadState['measure'];
+  onChange: UpdateUpload;
+};
+
+function Measure(priceProps: Props) {
+  const { data, state, onChange } = priceProps;
+  const handleInput = useDebounceInput(onChange, 200);
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>, subType: string) => {
+      // Todo 제네릭화
+      const value = filterHeight(e.target.value);
+      e.target.value = value;
+      handleInput(+value, 'measure', subType);
+    },
+    [handleInput],
+  );
+
+  return (
+    <InfoArticle label="실측 사이즈">
+      {data.map(({ name, code }) => {
+        return (
+          <TextInput
+            key={name}
+            label={name}
+            subType={code}
+            postLabel="cm"
+            controlled={false}
+            value={state[code].toString()}
+            placeholder="수치 입력"
+            onChange={(e) => handleChange(e, code)}
+            className={$['measure-element']}
+          />
+        );
+      })}
+    </InfoArticle>
+  );
+}
+
+export default memo(Measure);
