10a2e9a4b2c49f6db31c6d276c5bded8e2c77cf5
동동이 <drdragon777@naver.com>
2022-08-21 02:57:46 +0900

    soma-279/feat - add upload api connection

--- api/upload.ts
+++ api/upload.ts
@@ -1,21 +1,61 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useMutation } from 'react-query';
 
-import { AiAxios } from 'lib/axios';
+import { UploadStoreState } from '#types/storeType/upload';
+import { AiAxios, Axios } from 'lib/axios';
+import { deepClone, arrToString } from 'utils';
 
 export const postImgs = async (files: FormData) => {
   const { data } = await AiAxios.post('/api/product/uploads', files);
   return data;
 };
 
+export const postProduct = async (
+  uploadData: UploadStoreState,
+): Promise<res.uploadData> => {
+  const {
+    imgUpload,
+    removeImg,
+    updateUpload,
+    clearMeasure,
+    clearUpload,
+    ...rest
+  }: UploadStoreState = deepClone(uploadData);
+
+  const reqData: req.uploadData = {
+    ...rest,
+    imgList: rest.imgList.map(({ src }) => src),
+    basicInfo: {
+      title: rest.basicInfo.title,
+      category: arrToString(rest.basicInfo.category),
+      brand: rest.basicInfo.brand,
+    },
+    style: {
+      ...rest.style,
+      color: arrToString(rest.style.color),
+    },
+  };
+
+  Object.entries(reqData.measure).forEach(([key, value]) => {
+    if (!value) delete reqData.measure[key];
+  });
+
+  const { data } = await Axios.post('/api/product', reqData);
+  return data;
+};
+
 export const useImgUpload = () => {
-  return useMutation(
-    (files: FormData) => {
-      return postImgs(files);
+  return useMutation(postImgs, {
+    onError: (err) => {
+      console.log(err);
     },
-    {
-      onError: (err) => {
-        console.log(err);
-      },
+  });
+};
+
+export const useProductUpload = () => {
+  return useMutation(postProduct, {
+    onError: (err) => {
+      console.log(err);
     },
-  );
+  });
 };
