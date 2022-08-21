46d952398cd9a1ba41c20bfc8290f8bc3f8544f3
동동이 <drdragon777@naver.com>
2022-08-21 16:53:47 +0900

    soma-279/feat - add productDetail apis

---
+++ api/product.ts
@@ -0,0 +1,17 @@
+import { useQuery } from 'react-query';
+
+import { Axios } from 'lib/axios';
+
+export const getProductDetail = async (
+  id: string,
+): Promise<res.ProductDetail> => {
+  const { data } = await Axios.get<res.ProductDetail>(
+    `/api/product/detail/${id}`,
+  );
+  return data;
+};
+
+export function useProdutDetail(id: string) {
+  const response = useQuery(['product', id], () => getProductDetail(id));
+  return response;
+}
---
+++ types/apiTypes/product.d.ts
@@ -0,0 +1,47 @@
+declare namespace res {
+  type ProductDetail = {
+    status: number;
+    data: {
+      isMe: boolean;
+      status: string;
+      sellerInfo: {
+        profileImg: string;
+        nickname: string;
+        image: string[];
+      };
+      basic: {
+        title: string;
+        classification: string;
+        brand: string;
+        productInfo: string;
+        styleInfo: string;
+      };
+      sellerNotice: {
+        condition: string;
+        pollution: string;
+        height: string;
+        length: string;
+        bodyForm: string;
+        fit: string;
+        purchaseTime: string;
+        purchasePlace: string;
+      };
+      measure: {
+        length: number;
+        shoulderWidth: number;
+        chestSection: number;
+        sleeveLength: number;
+        waistSection: number;
+        thighSection: number;
+        rise: number;
+        bottomSection: number;
+      };
+      opinion: string;
+      price: number;
+      isIncludeDelivery: boolean;
+      updatedAt: string;
+      like: number;
+      views: number;
+    };
+  };
+}
