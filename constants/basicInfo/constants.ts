ab68e236ef2918e628d7fcde068708bc9e583830
동동이 <drdragon777@naver.com>
2022-08-24 01:45:14 +0900

    chore - delete client data from constants

--- constants/basicInfo/constants.ts
+++ constants/basicInfo/constants.ts
@@ -1,33 +1,25 @@
-import { DefaultData } from '#types/index';
 import { BasicUserInfo, btnTemplateBox } from '#types/info';
-import { bodyShapes, bottomSizes, genders, topSizes } from '@constants/basic';
 
-type btnBox = btnTemplateBox<keyof BasicUserInfo, undefined> & {
-  datas: (string | DefaultData)[];
-};
+type btnBox = btnTemplateBox<keyof BasicUserInfo, undefined>;
 
 const basicBtnProps: btnBox[] = [
   {
     label: '성별',
     type: 'gender',
-    datas: genders,
     required: true,
   },
   {
     label: '체형',
     type: 'bodyShape',
-    datas: bodyShapes,
     required: true,
   },
   {
     label: '상의 사이즈',
     type: 'topSize',
-    datas: topSizes,
   },
   {
     label: '하의 사이즈(인치)',
     type: 'bottomSize',
-    datas: bottomSizes,
   },
 ];
 
