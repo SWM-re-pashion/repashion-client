21e3122fef55f07eea19277f3e70a31d3ce4218d
동동이 <drdragon777@naver.com>
2022-08-27 00:09:55 +0900

    refactor - refactoring useDragScroll

--- hooks/useDragScroll.ts
+++ hooks/useDragScroll.ts
@@ -1,19 +1,20 @@
 import { RefObject, useEffect } from 'react';
 
 export default function useDragScroll(dragRef: RefObject<HTMLElement>) {
+  const ref = dragRef.current;
   let isDown = false;
   let startX: number;
   let startScrollLeft: number;
 
+  const getPageX = (e: MouseEvent | TouchEvent) => {
+    if ('touches' in e) return e.touches[0].pageX;
+    return e.pageX;
+  };
+
   const mouseTouchDown = (e: MouseEvent | TouchEvent) => {
-    const ref = dragRef.current;
     if (ref) {
       isDown = true;
-      if (e.type === 'mousedown' && 'pageX' in e) {
-        startX = e.pageX - ref.offsetLeft;
-      } else if (e.type === 'touchstart' && 'touches' in e) {
-        startX = e.touches[0].pageX - ref.offsetLeft;
-      }
+      startX = getPageX(e) - ref.offsetLeft;
       startScrollLeft = ref.scrollLeft;
     }
   };
@@ -24,35 +25,30 @@ export default function useDragScroll(dragRef: RefObject<HTMLElement>) {
     isDown = false;
   };
   const mouseTouchMove = (e: MouseEvent | TouchEvent) => {
-    const ref = dragRef.current;
-
     if (!isDown) return;
     if (e.cancelable) e.preventDefault();
 
     if (ref) {
-      if (e.type === 'mousemove' && 'pageX' in e) {
-        const currentX = e.pageX - ref.offsetLeft;
-        const walk = currentX - startX;
-        ref.scrollLeft = startScrollLeft - walk;
-      } else if (e.type === 'touchmove' && 'touches' in e) {
-        const currentX = e.touches[0].pageX - ref.offsetLeft;
-        const walk = currentX - startX;
-        ref.scrollLeft = startScrollLeft - walk;
-      }
+      const currentX = getPageX(e) - ref.offsetLeft;
+      const walk = currentX - startX;
+      ref.scrollLeft = startScrollLeft - walk;
     }
   };
 
   useEffect(() => {
-    const ref = dragRef.current;
     if (ref) {
       ref.addEventListener('mousedown', mouseTouchDown);
       ref.addEventListener('mouseleave', mouseTouchLeave);
       ref.addEventListener('mouseup', mouseTouchUp);
       ref.addEventListener('mousemove', mouseTouchMove);
-      ref.addEventListener('touchstart', mouseTouchDown);
+      ref.addEventListener('touchstart', mouseTouchDown, {
+        passive: true,
+      });
       ref.addEventListener('touchcancel', mouseTouchLeave);
       ref.addEventListener('touchend', mouseTouchUp);
-      ref.addEventListener('touchmove', mouseTouchMove);
+      ref.addEventListener('touchmove', mouseTouchMove, {
+        passive: true,
+      });
     }
 
     return () => {
