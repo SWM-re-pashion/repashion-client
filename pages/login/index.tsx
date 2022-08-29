65e7f4370952c000280b2dd9934393270dde77c2
동동이 <drdragon777@naver.com>
2022-08-30 01:59:46 +0900

    feat - add seo data & icons

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,11 +3,13 @@ import { useRouter } from 'next/router';
 
 import { ReactElement } from 'react';
 
+import HeadMeta from '@atoms/HeadMeta';
 import { Ellipse, Google, Kakao, Polygon, Rectangle, Star } from '@atoms/icon';
 import FooterWrapper from '@molecules/FooterWrapper';
 import { SocialLoginBtn } from '@molecules/SocialLoginBtn';
 import Layout from '@templates/Layout';
 import classnames from 'classnames';
+import { data } from 'constants/seo';
 
 import { NextPageWithLayout } from '../_app';
 import $ from './style.module.scss';
@@ -24,37 +26,45 @@ const Login: NextPageWithLayout = () => {
   };
 
   return (
-    <section className={$['on-boarding']}>
-      <Ellipse className={classnames($.icon, $.ellipse)} />
-      <Star className={classnames($.icon, $.star)} />
-      <Polygon className={classnames($.icon, $.polygon)} />
-      <Rectangle className={classnames($.icon, $.rectangle)} />
-      <h1 className={$.title}>re:Fashion</h1>
-      <span className={$.decription}>
-        recommend individual outfit from secondhand
-      </span>
+    <>
+      <HeadMeta
+        title={data.title}
+        image={data.images.logo}
+        description={data.description}
+        url={data.url}
+        creator={data.creator}
+      />
+      <section className={$['on-boarding']}>
+        <Ellipse className={classnames($.icon, $.ellipse)} />
+        <Star className={classnames($.icon, $.star)} />
+        <Polygon className={classnames($.icon, $.polygon)} />
+        <Rectangle className={classnames($.icon, $.rectangle)} />
+        <h1 className={$.title}>re:Fashion</h1>
+        <span className={$.decription}>
+          recommend individual outfit from secondhand
+        </span>
 
-      <FooterWrapper>
-        <SocialLoginBtn
-          className={$['login-btn']}
-          Logo={<Kakao style={{ padding: '6px' }} />}
-          onClick={loginWithKakao}
-          text="카카오 로그인"
-          backgroundColor="#fee500"
-          borderRadius="12px"
-          hasBtnPadding
-        />
-        <SocialLoginBtn
-          className={classnames($['login-btn'], $['google-login'])}
-          Logo={<Google style={{ padding: '6px' }} />}
-          onClick={loginWithGoogle}
-          text="Google 계정으로 로그인"
-          backgroundColor="#ffffff"
-          borderRadius="12px"
-          fontWeight={700}
-          hasBtnPadding
-        />
-        {/* <SocialLoginBtn
+        <FooterWrapper>
+          <SocialLoginBtn
+            className={$['login-btn']}
+            Logo={<Kakao style={{ padding: '6px' }} />}
+            onClick={loginWithKakao}
+            text="카카오 로그인"
+            backgroundColor="#fee500"
+            borderRadius="12px"
+            hasBtnPadding
+          />
+          <SocialLoginBtn
+            className={classnames($['login-btn'], $['google-login'])}
+            Logo={<Google style={{ padding: '6px' }} />}
+            onClick={loginWithGoogle}
+            text="Google 계정으로 로그인"
+            backgroundColor="#ffffff"
+            borderRadius="12px"
+            fontWeight={700}
+            hasBtnPadding
+          />
+          {/* <SocialLoginBtn
           className={$['login-btn']}
           src="/images/apple_login_btn.png"
           alt="애플 로그인"
@@ -63,8 +73,9 @@ const Login: NextPageWithLayout = () => {
           backgroundColor="#ffffff"
           borderRadius="12px"
         /> */}
-      </FooterWrapper>
-    </section>
+        </FooterWrapper>
+      </section>
+    </>
   );
 };
 
