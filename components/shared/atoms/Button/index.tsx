ba022777a91cff67bd7aeb567adbfc5d4aa2d484
동동이 <drdragon777@naver.com>
2022-09-01 22:43:01 +0900

    feat - add Button, Span prop

--- components/shared/atoms/Button/index.tsx
+++ components/shared/atoms/Button/index.tsx
@@ -9,19 +9,26 @@ type Props = {
   label?: string;
   iconBtn?: boolean;
   background?: string;
+  borderRadius?: string;
   onClick?: () => void;
   errorMsg?: string;
 } & DefaultProps;
 
 export default function Button(btnProps: Props) {
-  const { color, fontWeight } = btnProps;
+  const { color, fontWeight, borderRadius } = btnProps;
   const { label, iconBtn, background, onClick, errorMsg } = btnProps;
   const { className, style, children } = btnProps;
   return (
     <button
       type="button"
       onClick={onClick}
-      style={{ ...style, color, fontWeight, backgroundColor: background }}
+      style={{
+        ...style,
+        color,
+        fontWeight,
+        backgroundColor: background,
+        borderRadius,
+      }}
       className={classnames(
         $.btn,
         className,
--- components/shared/atoms/Span/index.tsx
+++ components/shared/atoms/Span/index.tsx
@@ -8,16 +8,19 @@ type Props = {
   color?: string;
   fontSize?: number;
   fontWeight?: number;
+  isStrongFontFamily?: boolean;
 } & DefaultProps;
 
 export default function Span(labelProps: Props) {
-  const { className, style, color, fontSize, fontWeight, children } =
-    labelProps;
+  const { className, style, children } = labelProps;
+  const { color, fontSize, fontWeight, isStrongFontFamily } = labelProps;
 
   return (
     <span
       style={{ ...style, color, fontWeight }}
-      className={classNames($.label, className, font(fontSize))}
+      className={classNames($.label, className, font(fontSize), {
+        [$['strong-font-family']]: isStrongFontFamily,
+      })}
     >
       {children}
     </span>
