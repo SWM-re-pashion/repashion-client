197f8fb0d0b8b61be0b911355411a70155b3d344
동동이 <drdragon777@naver.com>
2022-09-01 22:43:21 +0900

    feat - add isGender prop

--- components/shared/molecules/SelectBox/index.tsx
+++ components/shared/molecules/SelectBox/index.tsx
@@ -1,7 +1,8 @@
 import { memo, useRef } from 'react';
 
 import { DefaultData } from '#types/index';
-import { SelectArrow } from '@atoms/icon';
+import { Check, SelectArrow } from '@atoms/icon';
+import Span from '@atoms/Span';
 import classnames from 'classnames';
 
 import $ from './style.module.scss';
@@ -16,14 +17,21 @@ type Props<T, U> = {
   type?: T;
   subType?: U;
   width?: string;
+  height?: string;
+  isGender?: boolean;
 };
 
 function SelectBox<T, U>(selectProps: Props<T, U>) {
-  const { options, selected, onChange } = selectProps;
-  const { name, width, type, subType } = selectProps;
+  const { options, selected, onChange, isGender } = selectProps;
+  const { name, width, height, type, subType } = selectProps;
   const labelRef = useRef<HTMLButtonElement>(null);
   const [isClicked, setIsClicked] = useSelect(labelRef);
   const labelName = getLabelName(options, selected);
+
+  const isGenderSelected = (optionName: string) =>
+    isGender && labelName === optionName;
+  const isSelected = (optionName: string) =>
+    !isGender && labelName === optionName;
 
   const handleMouseDown = (
     e: React.MouseEvent<HTMLElement> | React.TouchEvent<HTMLElement>,
@@ -48,8 +56,9 @@ function SelectBox<T, U>(selectProps: Props<T, U>) {
     <div
       className={classnames($['select-box'], {
         [$['select-box-clicked']]: isClicked,
+        [$['gender-box']]: isGender,
       })}
-      style={{ ...{ width } }}
+      style={{ ...{ width, height } }}
     >
       <button
         id={`${name}-select-box`}
@@ -60,7 +69,13 @@ function SelectBox<T, U>(selectProps: Props<T, U>) {
         aria-controls={`${name}-select-list`}
         onClick={handleMouseDown}
       >
-        {labelName || '선택'}
+        <Span
+          className={classnames({
+            [$['gender-text']]: isGender,
+          })}
+        >
+          {labelName || '선택'}
+        </Span>
         <SelectArrow
           className={classnames($.arrow, {
             [$['arrow-clicked']]: isClicked,
@@ -74,6 +89,7 @@ function SelectBox<T, U>(selectProps: Props<T, U>) {
           aria-labelledby={`${name}-select-box`}
           role="menu"
           tabIndex={0}
+          style={{ top: height ? `calc(${height} + 6px)` : '56px' }}
           className={classnames($['select-wrapper'], {
             [$['select-wrapper-clicked']]: isClicked,
           })}
@@ -89,13 +105,20 @@ function SelectBox<T, U>(selectProps: Props<T, U>) {
                 tabIndex={0}
                 role="menuitem"
                 key={optionName}
+                style={{ ...{ height } }}
                 className={classnames($['select-item'], {
-                  [$['select-item-clicked']]: labelName === optionName,
+                  [$['select-item-clicked']]: isSelected(optionName),
+                  [$['select-hover']]: !isGender,
+                  [$['gender-item-clicked']]: isGenderSelected(optionName),
+                  [$['gender-hover']]: isGender,
                 })}
                 onClick={(e) => handleSelectItem(e, optionData)}
                 onKeyPress={(e) => handleSelectItem(e, optionData)}
               >
                 {optionName}
+                {isGenderSelected(optionName) && (
+                  <Check className={$['check-icon']} />
+                )}
               </li>
             );
           })}
