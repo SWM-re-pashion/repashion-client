acc3f0284999d721b05e7322ec1112c1d9ee93f5
동동이 <drdragon777@naver.com>
2022-09-16 18:29:30 +0900

    feat - add Keyword molecule

---
+++ components/Search/molecules/Keyword/index.tsx
@@ -0,0 +1,35 @@
+import { memo, MouseEvent } from 'react';
+
+import { searchState } from '#types/storeType/search';
+import { ImgClose } from '@atoms/icon';
+import Span from '@atoms/Span';
+
+import $ from './style.module.scss';
+
+type Props = {
+  keyword: string;
+  removeKeyword: searchState['removeKeyword'];
+  queryFunc: (value: string) => Promise<boolean>;
+};
+
+function Keyword(inputProps: Props) {
+  const { keyword, removeKeyword, queryFunc } = inputProps;
+  const handleClick = () => queryFunc(keyword);
+  const handleRemove = (e: MouseEvent) => {
+    e.stopPropagation();
+    removeKeyword(keyword);
+  };
+
+  return (
+    <button type="button" className={$.keyword} onClick={handleClick}>
+      <Span className={$['keyword-text']} fontWeight={500}>
+        {keyword}
+      </Span>
+      <button type="button" onClick={handleRemove}>
+        <ImgClose fill="#936dff" />
+      </button>
+    </button>
+  );
+}
+
+export default memo(Keyword);
