e9f7226496329853efad4bf80a8bd330b5be8116
동동이 <drdragon777@naver.com>
2022-09-20 11:23:48 +0900

    feat - add ImgBtn props

--- components/Upload/molecules/ImgCard/index.tsx
+++ components/Upload/molecules/ImgCard/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { memo } from 'react';
 
 import { ImgClose } from '@atoms/icon';
+import classnames from 'classnames';
 
 import $ from './style.module.scss';
 
@@ -11,19 +12,36 @@ type Props = {
   first: boolean;
   src: string;
   remove: (deleteId: number) => void;
+  onClick?: (id: number) => void;
+  noMargin?: boolean;
+  btnColor?: string;
 };
 
 function ImgCard(imgProps: Props) {
-  const { id, first, src, remove } = imgProps;
-  const onClick = () => {
+  const { id, first, src, remove, onClick, noMargin, btnColor } = imgProps;
+  const removeImg = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     remove(id);
+  };
+  const handleClick = () => {
+    if (onClick) onClick(id);
   };
 
   return (
-    <div className={$['img-card']}>
+    <div
+      role="button"
+      tabIndex={0}
+      className={classnames($['img-card'], { [$['no-margin']]: noMargin })}
+      onClick={handleClick}
+      onKeyDown={handleClick}
+    >
       <Image src={src} alt={`${id}번째 사진`} width="106" height="106" />
-      <button type="button" className={$['img-close']} {...{ onClick }}>
-        <ImgClose />
+      <button
+        type="button"
+        className={$['img-close']}
+        {...{ onClick: removeImg }}
+      >
+        <ImgClose fill={btnColor} />
       </button>
       {first && (
         <div className={$.representative}>
