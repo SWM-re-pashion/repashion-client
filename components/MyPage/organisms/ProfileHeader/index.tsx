0edce88c8a01051b6eecdc017036f97b685c4a25
동동이 <drdragon777@naver.com>
2022-09-20 22:08:46 +0900

    feat - add ProfileImg prop

--- components/MyPage/organisms/ProfileHeader/index.tsx
+++ components/MyPage/organisms/ProfileHeader/index.tsx
@@ -15,13 +15,13 @@ type Props = {
 function ProfileHeader(headerProps: Props) {
   const { nickname, isMe } = headerProps;
   return (
-    <header className={$['profile-header']}>
+    <header className={$['profile-header']} aria-label="프로필 헤더 바">
       <div className={$['profile-header-wrapper']}>
         <Span fontSize={18} className={$['profile-name']}>
           {isMe ? '내' : nickname} 프로필
         </Span>
         {isMe && (
-          <Button iconBtn className={$['setting-btn']}>
+          <Button iconBtn className={$['setting-btn']} label="설정 버튼">
             <Setting />
           </Button>
         )}
