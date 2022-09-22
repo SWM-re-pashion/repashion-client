6cb54e59e99cfadb16a4e28d130f5950c1ea797d
동동이 <drdragon777@naver.com>
2022-09-22 22:18:03 +0900

    refactor - add mypage

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -2,18 +2,8 @@ import { GetServerSidePropsContext } from 'next';
 
 import { ReactElement } from 'react';
 
-import HeadMeta from '@atoms/HeadMeta';
-import { seoData } from '@constants/seo';
-import { statusData } from '@constants/status';
-import Footer from '@organisms/Footer';
 import Layout from '@templates/Layout';
-import ProfileHeader from 'components/MyPage/organisms/ProfileHeader';
-import StatusMenuList from 'components/MyPage/organisms/StatusMenuList';
-import UserProfile from 'components/MyPage/organisms/UserProfile';
-import ProductItemList from 'components/Shop/Organisms/ProductItemList';
-import { useQueryRouter, useSearch } from 'hooks';
-
-import $ from './style.module.scss';
+import ProfileTemplate from '@templates/ProfileTemplate';
 
 export async function getServerSideProps({ query }: GetServerSidePropsContext) {
   const { status } = query;
@@ -27,38 +17,15 @@ export async function getServerSideProps({ query }: GetServerSidePropsContext) {
 }
 
 function MyPage({ status }: { status: string | null }) {
-  const statusQuery = useSearch('status');
-  const queryStatus = useQueryRouter('status');
-  const selectedMenu = statusQuery || status || statusData[0].code;
-
   return (
-    <>
-      <HeadMeta
-        title="re:Fashion | 마이 페이지"
-        url={`${seoData.url}/mypage`}
-      />
-      <ProfileHeader nickname="bruney" isMe />
-      <section className={$['mypage-body']}>
-        <UserProfile
-          profile={{
-            profileImg:
-              'https://user-images.githubusercontent.com/62797441/187207056-de246ecf-c46a-4a41-b7e6-8e3d760aae99.svg',
-            nickname: 'bruney',
-          }}
-          totalDealNum={13}
-        />
-
-        <StatusMenuList
-          data={statusData}
-          selectedMenu={selectedMenu}
-          onClick={queryStatus}
-        />
-      </section>
-
-      <ProductItemList paddingTop="0" paddingBottom="80px" />
-
-      <Footer />
-    </>
+    <ProfileTemplate
+      {...{ status, isMe: true, isNeedFooter: true }}
+      profile={{
+        profileImg:
+          'https://user-images.githubusercontent.com/62797441/187207056-de246ecf-c46a-4a41-b7e6-8e3d760aae99.svg',
+        nickname: 'bruney',
+      }}
+    />
   );
 }
 
