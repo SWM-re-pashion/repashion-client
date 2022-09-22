77ccb4e9eee04ae4bcc2cd30f54d50c658175890
동동이 <drdragon777@naver.com>
2022-09-23 05:06:11 +0900

    feat - add ProfileImgUpload organism

---
+++ components/MyPage/organisms/ProfileImgUpload/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { Camera } from '@atoms/icon/Camera';
+import InputFile from '@atoms/InputFile';
+import InputLabel from '@atoms/InputLabel';
+import LoadingSpinner from '@atoms/LoadingSpinner';
+import ProfileImg from '@atoms/ProfileImg';
+
+import $ from './style.module.scss';
+
+type Props = {
+  img: string;
+  inputRef: React.RefObject<HTMLInputElement>;
+  onUploadClick: () => void;
+  handleUpload: (e: React.ChangeEvent) => void;
+};
+
+function ProfileImgUpload(uploadProps: Props) {
+  const { img, inputRef, onUploadClick, handleUpload } = uploadProps;
+  return (
+    <div
+      role="button"
+      tabIndex={0}
+      className={$['profile-img']}
+      onClick={onUploadClick}
+      onKeyDown={onUploadClick}
+    >
+      {/* <LoadingSpinner width={50} borderWidth={3} color="#876bf6" /> */}
+      <ProfileImg
+        width={180}
+        height={180}
+        src={img}
+        alt="업로드할 내 프로필 사진"
+      />
+      <div className={$['camera-btn']}>
+        <Camera />
+      </div>
+
+      <InputLabel htmlFor="chooseFile" text="프로필 이미지 업로드" />
+      <InputFile
+        ref={inputRef}
+        id="chooseFile"
+        accept=".jpg,.jpeg,.png,.webp,.heif,.heic,.bmp"
+        onChange={handleUpload}
+        none
+        isMultiple={false}
+      />
+    </div>
+  );
+}
+
+export default ProfileImgUpload;
