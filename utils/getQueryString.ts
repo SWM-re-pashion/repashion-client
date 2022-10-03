fa64dbfb37374d1f297da6d044d1f77665c62eb0
동동이 <drdragon777@naver.com>
2022-10-04 01:41:33 +0900

    feat - add getQueryStringObj, getQueriesArr

--- utils/getQueryString.ts
+++ utils/getQueryString.ts
@@ -1,5 +1,25 @@
+import { UrlQuery } from '#types/index';
+
 function getQueryString(queryObj: { [key: string]: string }) {
   return decodeURIComponent(new URLSearchParams(queryObj).toString());
 }
 
-export default getQueryString;
+function getQueryStringObj<T extends string>(
+  queries: [T, string][],
+): Record<T, string> {
+  return queries.reduce((acc, [key, value]) => {
+    return { ...acc, [key]: value };
+  }, {} as Record<T, string>);
+}
+
+function getQueriesArr<T>(
+  datas: [T, string?][],
+  urlQueries: UrlQuery[],
+): [T, string][] {
+  return datas.map(([key, value], i) => [
+    key,
+    (urlQueries[i] || value || '').toString(),
+  ]);
+}
+
+export { getQueryString, getQueryStringObj, getQueriesArr };
