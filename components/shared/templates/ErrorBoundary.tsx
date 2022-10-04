ec650750cda7340280a10f0dd0a27448f53046b8
동동이 <drdragon777@naver.com>
2022-10-05 07:30:58 +0900

    feat - add ErrorBoundary

---
+++ components/shared/templates/ErrorBoundary.tsx
@@ -0,0 +1,80 @@
+import React, { ReactElement } from 'react';
+
+type ErrorFallbackProps<ErrorType extends Error = Error> = {
+  error: ErrorType;
+  reset: (...args: unknown[]) => void;
+};
+
+type ErrorFallbackType = <ErrorType extends Error>(
+  props: ErrorFallbackProps<ErrorType>,
+) => React.ReactNode;
+
+type Props = {
+  resetQuery?: () => void;
+  errorFallback: ErrorFallbackType;
+  children: ReactElement;
+  keys?: unknown[];
+};
+
+type State = {
+  hasError: boolean;
+  error: Error | null;
+};
+const initialState = { hasError: false, error: null };
+
+const changedArray = (
+  prevArray: Array<unknown> = [],
+  nextArray: Array<unknown> = [],
+) => {
+  return (
+    prevArray.length !== nextArray.length ||
+    prevArray.some((item, index) => {
+      return !Object.is(item, nextArray[index]);
+    })
+  );
+};
+
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error };
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    const { error } = this.state;
+    const { keys } = this.props;
+
+    if (
+      error !== null &&
+      prevState.error !== null &&
+      changedArray(prevProps.keys, keys)
+    ) {
+      this.resetBoundary();
+    }
+  }
+
+  resetBoundary = () => {
+    const { resetQuery } = this.props;
+    resetQuery?.();
+    this.setState(initialState);
+  };
+
+  render() {
+    const { hasError, error } = this.state;
+    const { errorFallback } = this.props;
+    const { children } = this.props;
+
+    if (hasError && error !== null) {
+      return errorFallback({
+        error,
+        reset: this.resetBoundary,
+      });
+    }
+
+    return children;
+  }
+}
