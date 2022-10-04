b8a8f050be3bd5bd38a7bb61da30121cd8770e96
동동이 <drdragon777@naver.com>
2022-10-05 07:30:48 +0900

    feat - add AsyncBoundary

---
+++ components/shared/templates/AsyncBoundary.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, PropsWithChildren, useCallback } from 'react';
+import { useQueryErrorResetBoundary } from 'react-query';
+
+import ErrorBoundary from './ErrorBoundary';
+import SSRSafeSuspense from './SSRSafeSuspense';
+
+type ErrorBoundaryProps = ComponentProps<typeof ErrorBoundary>;
+
+type Props = {
+  suspenseFallback: ComponentProps<typeof SSRSafeSuspense>['fallback'];
+  errorFallback: ErrorBoundaryProps['errorFallback'];
+  keys?: Array<unknown>;
+};
+
+function AsyncBoundary(props: PropsWithChildren<Props>) {
+  const { suspenseFallback, errorFallback, children, keys } = props;
+  const { reset } = useQueryErrorResetBoundary();
+  const resetHandler = useCallback(() => {
+    reset();
+  }, [reset]);
+
+  return (
+    <ErrorBoundary resetQuery={resetHandler} {...{ errorFallback, keys }}>
+      <SSRSafeSuspense fallback={suspenseFallback}>{children}</SSRSafeSuspense>
+    </ErrorBoundary>
+  );
+}
+
+export default AsyncBoundary;
