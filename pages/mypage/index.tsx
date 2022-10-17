d3c5cf0efea873b54679bf87ffd05d0257b323d9
동동이 <drdragon777@naver.com>
2022-10-17 17:40:53 +0900

    feat - add HOC

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -6,23 +6,26 @@ import { queryKey } from '@constants/react-query';
 import { dehydrate, QueryClient } from '@tanstack/react-query';
 import Layout from '@templates/Layout';
 import ProfileTemplate from '@templates/ProfileTemplate';
+import { withGetServerSideProps } from 'api/core/withGetServerSideProps';
 import { getMyInfo } from 'api/profile';
 import { useMyInfo } from 'hooks/api/profile';
 
-export async function getServerSideProps({ query }: GetServerSidePropsContext) {
-  const { status } = query;
-  const statusQuery = (typeof status !== 'object' && status) || '';
+export const getServerSideProps = withGetServerSideProps(
+  async ({ query }: GetServerSidePropsContext) => {
+    const { status } = query;
+    const statusQuery = (typeof status !== 'object' && status) || '';
 
-  const queryClient = new QueryClient();
-  await queryClient.fetchQuery(queryKey.myInfo, () => getMyInfo());
+    const queryClient = new QueryClient();
+    await queryClient.fetchQuery(queryKey.myInfo, () => getMyInfo());
 
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-      status: statusQuery,
-    },
-  };
-}
+    return {
+      props: {
+        dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+        status: statusQuery,
+      },
+    };
+  },
+);
 
 function MyPage({ status }: { status: string }) {
   const { data } = useMyInfo();
