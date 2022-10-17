e5b8113528328db6f07f09b1faecf60c540e8f89
동동이 <drdragon777@naver.com>
2022-10-17 10:57:49 +0900

    feat - add mypage api

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -2,29 +2,41 @@ import { GetServerSidePropsContext } from 'next';
 
 import { ReactElement } from 'react';
 
+import { queryKey } from '@constants/react-query';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import Layout from '@templates/Layout';
 import ProfileTemplate from '@templates/ProfileTemplate';
+import { getMyInfo } from 'api/profile';
+import { useMyInfo } from 'hooks/api/profile';
 
 export async function getServerSideProps({ query }: GetServerSidePropsContext) {
   const { status } = query;
   const statusQuery = (typeof status !== 'object' && status) || '';
 
+  const queryClient = new QueryClient();
+  await queryClient.fetchQuery(queryKey.myInfo, () => getMyInfo());
+
   return {
     props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
       status: statusQuery,
     },
   };
 }
 
 function MyPage({ status }: { status: string }) {
+  const { data } = useMyInfo();
+  const profile = data?.data;
+
+  if (!profile) return null;
   return (
     <ProfileTemplate
       {...{ status, isMe: true, isNeedFooter: true }}
       profile={{
-        profileImg:
-          'https://user-images.githubusercontent.com/62797441/187207056-de246ecf-c46a-4a41-b7e6-8e3d760aae99.svg',
-        nickname: 'bruney',
+        profileImage: profile?.profileImage,
+        name: profile?.name,
       }}
+      totalCount={profile?.totalCount}
     />
   );
 }
