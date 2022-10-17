b31f60735fb13bc9d40ecea82121964ce181a04e
동동이 <drdragon777@naver.com>
2022-10-17 10:32:44 +0900

    refactor - 서비스 준비중 표시

--- components/shared/templates/ProfileTemplate/index.tsx
+++ components/shared/templates/ProfileTemplate/index.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 import { Setting } from '@atoms/icon';
+import Span from '@atoms/Span';
 import { seoData } from '@constants/seo';
 import { statusData } from '@constants/status';
 import PageTemplate from '@templates/PageTemplate';
 import StatusMenuList from 'components/MyPage/organisms/StatusMenuList';
 import UserProfile from 'components/MyPage/organisms/UserProfile';
-import ProductItemList from 'components/Shop/Organisms/ProductItemList';
 import { useQueryRouter, useSearch } from 'hooks';
 
 import $ from './style.module.scss';
@@ -24,7 +24,9 @@ type Props = {
 function ProfileTemplate({ status, isMe, profile, isNeedFooter }: Props) {
   const statusQuery = useSearch('status');
   const queryStatus = useQueryRouter('status');
+  const replaceStatus = useQueryRouter('status', 'REPLACE');
   const selectedMenu = statusQuery || status || statusData[0].code;
+  const statusQueryFunc = statusQuery ? replaceStatus : queryStatus;
 
   return (
     <PageTemplate
@@ -44,7 +46,11 @@ function ProfileTemplate({ status, isMe, profile, isNeedFooter }: Props) {
           </Link>
         )
       }
-      outsideChildren={<ProductItemList paddingTop="0" paddingBottom="80px" />}
+      outsideChildren={
+        <div className={$.prepare}>
+          <Span>서비스 준비중입니다.</Span>
+        </div>
+      }
       isNeedFooter={isNeedFooter}
       paddingTop="84px"
       sidePadding="23px"
@@ -54,7 +60,7 @@ function ProfileTemplate({ status, isMe, profile, isNeedFooter }: Props) {
       <StatusMenuList
         data={statusData}
         selectedMenu={selectedMenu}
-        onClick={queryStatus}
+        onClick={statusQueryFunc}
       />
     </PageTemplate>
   );
