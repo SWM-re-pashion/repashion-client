f69032ada1bfb626f7a36e1f744e06ccc4f3f2d0
동동이 <drdragon777@naver.com>
2022-10-24 21:16:49 +0900

    chore - delete unneccesary file

--- shared/OAuth.ts
+++
@@ -1,3 +0,0 @@
-const REDIRECT_URI = 'http://localhost:3001/oauth';
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REST_API_KEY}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code`;
-export { REDIRECT_URI, KAKAO_AUTH_URL };
