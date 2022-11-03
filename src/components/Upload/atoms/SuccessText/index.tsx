3825ba08d370cc567f30747dddde898dc8bbdf22
동동이 <drdragon777@naver.com>
2022-11-04 02:34:30 +0900

    hotfix - add fontWeight

--- src/components/Upload/atoms/SuccessText/index.tsx
+++ src/components/Upload/atoms/SuccessText/index.tsx
@@ -9,13 +9,14 @@ function SuccessText(props: Props) {
   const { tag, color, material, category } = props;
   return (
     <Span fontWeight={500}>
-      스타일: <Span fontWeight={700}>{tag}</Span>
+      <Span fontWeight={650}>스타일</Span>: <Span fontWeight={700}>{tag}</Span>
       <br />
-      컬러: <Span fontWeight={700}>{color}</Span>
-      <br /> 소재: <Span fontWeight={700}>{material}</Span>
+      <Span fontWeight={650}>컬러</Span>: <Span fontWeight={700}>{color}</Span>
+      <br /> <Span fontWeight={650}>소재</Span>:{' '}
+      <Span fontWeight={700}>{material}</Span>
       <br />
-      카테고리: <Span fontWeight={700}>{category}</Span>가 성공적으로
-      인식되었습니다.
+      <Span fontWeight={650}>카테고리</Span>:{' '}
+      <Span fontWeight={700}>{category}</Span>가 성공적으로 인식되었습니다.
       <br />
       <br />
       인식 결과가 예상과 다르다면 입력 항목을 직접 수정해주세요.
