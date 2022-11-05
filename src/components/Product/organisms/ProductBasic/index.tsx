d945f208f08b01cfe3f4d4a00930e1b5a4fd3e28
동동이 <drdragon777@naver.com>
2022-11-06 04:04:31 +0900

    feat - add sale status UI

--- src/components/Product/organisms/ProductBasic/index.tsx
+++ src/components/Product/organisms/ProductBasic/index.tsx
@@ -1,5 +1,6 @@
 import { ProductBasicInfo } from '#types/product';
 import { StyleProps } from '#types/props';
+import SelectBox from '@molecules/SelectBox';
 import { productBasicUtil } from 'src/utils/product';
 import { replace } from 'src/utils/replace';
 
@@ -8,19 +9,32 @@ import $ from './style.module.scss';
 
 type Props = {
   basic: ProductBasicInfo;
+  isSoldOut: boolean;
 } & StyleProps;
 
-export default function ProductBasic({ basic }: Props) {
+export default function ProductBasic({ basic, isSoldOut }: Props) {
   const { title, classification } = basic;
   const datas = productBasicUtil(basic);
+  const saleStatus = isSoldOut ? '판매완료' : '판매중';
 
   return (
     <>
       <header className={$['product-header']}>
-        <h1 className={$['product-title']}>{title}</h1>
-        <span className={$['product-category']}>
-          {replace(classification, '/', ' > ')}
-        </span>
+        <div>
+          <h1 className={$['product-title']}>{title}</h1>
+          <span className={$['product-category']}>
+            {replace(classification, '/', ' > ')}
+          </span>
+        </div>
+        <SelectBox
+          options={['판매중', '판매완료']}
+          selected={saleStatus}
+          name="sale-status"
+          width="110px"
+          height="24px"
+          fontSize={14}
+          fontWeight={700}
+        />
       </header>
 
       <article>
