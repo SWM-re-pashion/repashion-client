c95df5c8d83e4e1511b5a3e78cbd395a5b95aef7
동동이 <drdragon777@naver.com>
2022-11-06 03:18:08 +0900

    hotfix - change response.data to response

--- src/api/core/refresh.ts
+++ src/api/core/refresh.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { ACCESSTOKEN, TOKEN_REFRESH } from '@constants/api';
-import { AxiosError, AxiosInstance } from 'axios';
+import { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { setAccessToken } from 'src/utils/auth';
 
 import { ForbiddenError } from './error';
@@ -10,21 +10,21 @@ export const refreshAccessToken = async (
   instance: AxiosInstance,
 ) => {
   try {
-    const response = await instance.get<res.reissue>(TOKEN_REFRESH);
+    const response = await instance.get<
+      res.reissue,
+      AxiosResponse<res.reissue, any>['data']
+    >(TOKEN_REFRESH);
     const {
       data: { accessToken },
-    } = response.data;
-    console.log(accessToken);
+    } = response;
     if (err.config.headers) {
       err.config.headers[ACCESSTOKEN] = accessToken;
     }
     setAccessToken(accessToken);
     instance.defaults.headers[ACCESSTOKEN] = accessToken;
-    response.headers['set-cookie'] = [`${ACCESSTOKEN}=${accessToken}`];
     const res = await instance.request(err.config);
-    return Promise.resolve(res.data);
+    return Promise.resolve(res);
   } catch (error) {
-    console.log(error);
     throw new ForbiddenError(403);
   }
 };
