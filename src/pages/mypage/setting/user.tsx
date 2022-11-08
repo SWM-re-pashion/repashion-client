fbe658a8668e0e618147ca99692e96332572ed2d
동동이 <drdragon777@naver.com>
2022-11-09 07:58:08 +0900

    chore - remove SSR function in user info update page

--- src/pages/mypage/setting/user.tsx
+++ src/pages/mypage/setting/user.tsx
@@ -1,29 +1,16 @@
 import React, { ReactElement } from 'react';
 
-import { queryKey } from '@constants/react-query';
-import { dehydrate, QueryClient } from '@tanstack/react-query';
+import Loading from '@atoms/Loading';
 import Layout from '@templates/Layout';
-import { withGetServerSideProps } from 'src/api/core/withGetServerSideProps';
-import { getMyInfo } from 'src/api/profile';
 import InfoModifyTemplate from 'src/components/MyPage/template/InfoModifyTemplate';
 import { useMyInfo } from 'src/hooks/api/profile';
-
-export const getServerSideProps = withGetServerSideProps(async () => {
-  const queryClient = new QueryClient();
-  await queryClient.fetchQuery(queryKey.myInfo, () => getMyInfo());
-
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-    },
-  };
-});
 
 function UserPage() {
   // TODO: 닉네임 중복체크
   const { data } = useMyInfo();
   const userData = data?.data;
-  if (!userData) return null;
+  if (!userData)
+    return <Loading style={{ height: 'calc(var(--vh, 1vh) * 100)' }} />;
   const { name, profileImage, email } = userData;
 
   return <InfoModifyTemplate {...{ name, profileImage, email }} />;
