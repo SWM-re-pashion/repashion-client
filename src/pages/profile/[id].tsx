36e0138cc76b031eb0ac457a528adaa63960a55c
동동이 <drdragon777@naver.com>
2022-11-09 07:13:55 +0900

    feat - remove SSR function in Profile page

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,34 +1,18 @@
-import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
 
 import { ReactElement } from 'react';
 
-import { queryKey } from '@constants/react-query';
-import { dehydrate, QueryClient } from '@tanstack/react-query';
 import Layout from '@templates/Layout';
 import ProfileTemplate from '@templates/ProfileTemplate';
-import { withGetServerSideProps } from 'src/api/core/withGetServerSideProps';
-import { getUserInfo } from 'src/api/profile';
 
-export const getServerSideProps = withGetServerSideProps(
-  async ({ params }: GetServerSidePropsContext) => {
-    const paramId = params?.id as string;
-
-    const queryClient = new QueryClient();
-    await queryClient.fetchQuery(queryKey.userInfo(paramId), () =>
-      getUserInfo(paramId),
-    );
-
-    return {
-      props: {
-        dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-        id: paramId,
-      },
-    };
-  },
-);
-
-function Profile({ id }: { id: string }) {
-  return <ProfileTemplate {...{ id, isMe: false, isNeedFooter: false }} />;
+function Profile() {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <ProfileTemplate
+      {...{ id: id as string, isMe: false, isNeedFooter: false }}
+    />
+  );
 }
 
 Profile.getLayout = function getLayout(page: ReactElement) {
