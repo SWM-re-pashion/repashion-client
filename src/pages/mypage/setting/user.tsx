a8539e7a149bda986e02cd724a33ce626c0ae821
동동이 <drdragon777@naver.com>
2022-11-09 09:37:50 +0900

    feat - add AsyncBoundary in setting/user page

--- src/pages/mypage/setting/user.tsx
+++ src/pages/mypage/setting/user.tsx
@@ -1,19 +1,23 @@
 import React, { ReactElement } from 'react';
 
+import ErrorFallback from '@atoms/ErrorFallback';
 import Loading from '@atoms/Loading';
+import AsyncBoundary from '@templates/AsyncBoundary';
 import Layout from '@templates/Layout';
 import InfoModifyTemplate from 'src/components/MyPage/template/InfoModifyTemplate';
-import { useMyInfo } from 'src/hooks/api/profile';
 
 function UserPage() {
   // TODO: 닉네임 중복체크
-  const { data } = useMyInfo();
-  const userData = data?.data;
-  if (!userData)
-    return <Loading style={{ height: 'calc(var(--vh, 1vh) * 100)' }} />;
-  const { name, profileImage, email } = userData;
-
-  return <InfoModifyTemplate {...{ name, profileImage, email }} />;
+  return (
+    <AsyncBoundary
+      suspenseFallback={
+        <Loading style={{ height: 'calc(var(--vh, 1vh) * 100)' }} />
+      }
+      errorFallback={ErrorFallback}
+    >
+      <InfoModifyTemplate />
+    </AsyncBoundary>
+  );
 }
 
 UserPage.getLayout = function getLayout(page: ReactElement) {
