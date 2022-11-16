640174589cbc27c99b3cd66701fb526540fad38f
동동이 <drdragon777@naver.com>
2022-11-16 11:49:05 +0900

    refactor - url query부분만 추출

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -3,20 +3,21 @@ import { useRouter } from 'next/router';
 import { getQueryStringObj, getQueriesArr, getPropFromQuery } from 'src/utils';
 
 function useSearch(target: string) {
-  return getPropFromQuery(useRouter().asPath.split('?')[1], target) || '';
+  const { pathname, asPath } = useRouter();
+  const query = asPath.replace(pathname, '');
+  return getPropFromQuery(query, target) || '';
 }
 
 function useMultipleSearch<T extends string>(
   queryDatas: [T, string?][],
   targets: readonly string[],
 ) {
-  const router = useRouter();
+  const { pathname, asPath } = useRouter();
+  const query = asPath.replace(pathname, '');
   return getQueryStringObj(
     getQueriesArr<T>(
       queryDatas,
-      targets.map(
-        (target) => getPropFromQuery(router.asPath.split('?')[1], target) || '',
-      ),
+      targets.map((target) => getPropFromQuery(query, target) || ''),
     ),
   );
 }
