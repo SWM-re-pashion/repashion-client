feae78bf00770204a2cc490d1f24b7ce500490f9
동동이 <drdragon777@naver.com>
2022-11-23 04:10:06 +0900

    feat - add mailto, sms regExp & logic

--- src/constants/link.ts
+++ src/constants/link.ts
@@ -1,1 +1,4 @@
-export const KAKAO_TALK_ONE_TO_ONE_CHAT = 'http://pf.kakao.com/_jNwnxj/chat';
+const KAKAO_TALK_ONE_TO_ONE_CHAT = 'http://pf.kakao.com/_jNwnxj/chat';
+const MAILTO = 'mailto:';
+const SMS = 'sms:';
+export { KAKAO_TALK_ONE_TO_ONE_CHAT, MAILTO, SMS };
--- src/utils/regExp.ts
+++ src/utils/regExp.ts
@@ -1,2 +1,6 @@
 export const isSameRegExpCondition = (condition: RegExp, str: string) =>
   condition.test(str);
+
+export const isSameMultipleRegExp = (regExps: RegExp[], str: string) => {
+  return regExps.map((regExp) => regExp.test(str));
+};
