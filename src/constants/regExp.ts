e4cb4556c9a77431eeeb7dca58619c6f588e0bcb
동동이 <drdragon777@naver.com>
2022-11-23 04:24:18 +0900

    fix - relative path regexp

--- src/constants/regExp.ts
+++ src/constants/regExp.ts
@@ -1,4 +1,4 @@
-const URL_REGEX = /^(https?:\/\/)?([da-z.-]+).([a-z.]{2,6})([/\w_.#-]*)*$/;
+const URL_REGEX = /^(https?:\/\/)([da-z.-]+).([a-z.]{2,6})([/\w_.#-]*)*$/;
 const PHONE_REGEX = /^\d{3,4}-?\d{3,4}-?\d{4}$/;
 const EMAIL_REGEX =
   /^[\da-zA-Z]([-_.]?[\da-zA-Z])*@[0-9a-zA-Z]([-_.]?[\da-zA-Z])*.[a-zA-Z]{2,3}$/i;
