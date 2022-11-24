b4e8a830b0425fe41fb0774b2dd909b582d67744
동동이 <drdragon777@naver.com>
2022-11-25 07:45:58 +0900

    feat - reflect useTouchScroll hook

--- src/components/Product/organisms/ProductRecommend/index.tsx
+++ src/components/Product/organisms/ProductRecommend/index.tsx
@@ -4,8 +4,8 @@ import { StyleProps } from '#types/props';
 import { NoProduct } from '@atoms/icon';
 import StrongLabel from 'src/components/Product/atoms/StrongLabel';
 import ProductItem from 'src/components/Shop/molecules/ProductItem';
-import { useDragScroll } from 'src/hooks';
 import { useProductRecommendItemList } from 'src/hooks/api/recommend';
+import useTouchScroll from 'src/hooks/useTouchScroll';
 
 import $ from './style.module.scss';
 
@@ -16,18 +16,20 @@ type Props = {
 export default function ProductRecommend({ id }: Props) {
   const { data } = useProductRecommendItemList(id);
   const ref = useRef<HTMLDivElement>(null);
-  useDragScroll(ref);
+  useTouchScroll(ref);
   const recommendData = data?.data;
   const isNoData = recommendData && recommendData.length === 0;
 
   return (
     <article className={$['product-recommend']}>
       <StrongLabel label="이런 옷이 어울릴 것 같아요" mid />
-      <div className={$['item-list']} ref={ref}>
-        {recommendData?.map((item) => (
-          <ProductItem isUnknown key={item.id} {...item} />
-        ))}
-        {isNoData && <NoProduct className={$['no-product']} />}
+      <div className={$['item-list-wrapper']}>
+        <div className={$['item-list']} ref={ref}>
+          {recommendData?.map((item) => (
+            <ProductItem isUnknown key={item.id} {...item} />
+          ))}
+          {isNoData && <NoProduct className={$['no-product']} />}
+        </div>
       </div>
     </article>
   );
