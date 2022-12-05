d29f86134e3bcbafa5b47eb8c5a7b1a64a91cbd0
동동이 <drdragon777@naver.com>
2022-12-05 18:23:18 +0900

    setting - add sass storybook additionalData setting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public'],
@@ -17,7 +19,54 @@ module.exports = {
   webpackFinal: async (baseConfig) => {
     const nextConfig = require('../next.config.js');
 
+    baseConfig.resolve.alias = {
+      ...baseConfig.resolve.alias,
+      src: path.resolve(__dirname, '../src'),
+      '@styles': path.resolve(__dirname, '../src/styles'),
+      '#types': path.resolve(__dirname, '../src/types'),
+      '@mocks': path.resolve(__dirname, '../__mocks__'),
+      '@constants': path.resolve(__dirname, '../src/constants'),
+      '@atoms': path.resolve(__dirname, '../src/components/shared/atoms'),
+      '@molecules': path.resolve(
+        __dirname,
+        '../src/components/shared/molecules',
+      ),
+      '@organisms': path.resolve(
+        __dirname,
+        '../src/components/shared/organisms',
+      ),
+      '@templates': path.resolve(
+        __dirname,
+        '../src/components/shared/templates',
+      ),
+    };
+
+    baseConfig.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      include: path.resolve(__dirname, '../src'),
+      use: [
+        {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('sass'),
+            sassOptions: {
+              includePaths: [path.appSrc + '/src'],
+            },
+            additionalData: `
+              @import "src/styles/main.scss";
+              @import "src/styles/_mixin.scss";
+              @import "src/styles/_typography.scss";
+              @import "src/styles/_grid.scss";
+              @import "src/styles/_loading.scss";
+            `,
+          },
+        },
+      ],
+    });
     // merge whatever from nextConfig into the webpack config storybook will use
-    return { ...baseConfig, ...nextConfig };
+    return {
+      ...baseConfig,
+      ...nextConfig,
+    };
   },
 };
