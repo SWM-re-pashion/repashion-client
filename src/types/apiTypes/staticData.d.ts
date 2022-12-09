748133adfcd939ba3e534630312f5411499d01dd
동동이 <drdragon777@naver.com>
2022-12-09 19:06:13 +0900

    fix - getStaticData return type

--- src/types/apiTypes/staticData.d.ts
+++ src/types/apiTypes/staticData.d.ts
@@ -12,14 +12,14 @@ declare namespace req {
 
 declare namespace res {
   type KindStaticData = {
-    staus: number;
+    status: number;
     data: {
       top: { name: string; code: string }[];
       bottom: { name: string; code: string }[];
     };
   };
   type StaticData = {
-    staus: number;
+    status: number;
     data: { name: string; code: string }[];
   };
 }
