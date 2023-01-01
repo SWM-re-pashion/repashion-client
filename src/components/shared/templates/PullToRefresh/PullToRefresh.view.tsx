eb444510aa71d86ef72ce7eb3daa5360c317e57b
동동이 <drdragon777@naver.com>
2023-01-02 02:59:38 +0900

    fix - remove unnecessary html tags

--- src/components/shared/templates/PullToRefresh/PullToRefresh.view.tsx
+++ src/components/shared/templates/PullToRefresh/PullToRefresh.view.tsx
@@ -1,25 +1,15 @@
 export type Props = {
-  containerRef: React.RefObject<HTMLDivElement>;
   pullDownRef: React.RefObject<HTMLDivElement>;
   childrenRef: React.RefObject<HTMLDivElement>;
   children: JSX.Element;
   refreshingContent: JSX.Element | string | null;
-  backgroundColor?: string;
-  className?: string;
 };
 
 function PullToRefreshView(viewProps: Props) {
-  const { containerRef, pullDownRef, childrenRef, children } = viewProps;
-  const { refreshingContent, backgroundColor, className } = viewProps;
+  const { pullDownRef, childrenRef, children } = viewProps;
+  const { refreshingContent } = viewProps;
   return (
-    <div
-      className={className}
-      style={{
-        backgroundColor,
-        position: 'relative',
-      }}
-      ref={containerRef}
-    >
+    <>
       <div
         ref={pullDownRef}
         style={{
@@ -33,14 +23,12 @@ function PullToRefreshView(viewProps: Props) {
       <div
         ref={childrenRef}
         style={{
-          position: 'absolute',
-          width: '100%',
           transition: 'transform 0.2s cubic-bezier(0, 0, 0.31, 1)',
         }}
       >
         {children}
       </div>
-    </div>
+    </>
   );
 }
 
