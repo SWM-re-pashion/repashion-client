c0ea11c3e908967e50a5fbcfa8b4190fe86c2fd0
동동이 <drdragon777@naver.com>
2023-06-04 22:19:59 +0900

    refactor - remove useMemo & move submit logic to Submit btn

--- src/components/shared/templates/UploadTemplate/index.tsx
+++ src/components/shared/templates/UploadTemplate/index.tsx
@@ -1,12 +1,9 @@
-import { FormEvent, useMemo } from 'react';
-
 import { UploadTemplateProps } from '#types/upload';
 import BackBtn from '@atoms/BackBtn';
 import HeadMeta from '@atoms/HeadMeta';
 import { seoData } from '@constants/seo';
 import { additionData } from '@constants/upload/constants';
 import { reviewData, sizeData, styleData } from '@constants/upload/utils';
-import ButtonFooter from '@molecules/ButtonFooter';
 import PageHeader from '@molecules/PageHeader';
 import { getBreadcrumb } from 'src/api/category';
 import AdditionInfo from 'src/components/Upload/organisms/AdditionInfo';
@@ -18,25 +15,17 @@ import Price from 'src/components/Upload/organisms/Price';
 import SellerReview from 'src/components/Upload/organisms/SellerReview';
 import SizeInfo from 'src/components/Upload/organisms/SizeInfo';
 import StyleSelect from 'src/components/Upload/organisms/StyleSelect';
-import { uploadStateToData } from 'src/helpers/upload';
+import SubmitBtn from 'src/components/Upload/organisms/SubmitBtn';
 import { useMounted } from 'src/hooks';
 import { useCategoryTree } from 'src/hooks/api/category';
 import { useStaticData } from 'src/hooks/api/staticData';
-import { useProductUpload, useUpdateProduct } from 'src/hooks/api/upload';
 import { useUploadUpdateStore } from 'src/hooks/useUploadUpdateStore';
 import { getJudgeCategory } from 'src/utils';
-import { toastError } from 'src/utils/toaster';
 
 import $ from './style.module.scss';
 
-type Props = {
-  id: string;
-} & UploadTemplateProps;
-
-function UploadTemplate({ id, isUpdate }: Props) {
+function UploadTemplate({ id, isUpdate }: UploadTemplateProps) {
   const isMounted = useMounted();
-  const { mutate } = useProductUpload();
-  const { mutate: updateMutate } = useUpdateProduct(id);
   const categoryData = useCategoryTree(true)?.data;
   const { data: styles } = useStaticData<res.StaticData>('Style');
   const { data: bodyShapes } = useStaticData<res.StaticData>('BodyShape');
@@ -50,43 +39,19 @@ function UploadTemplate({ id, isUpdate }: Props) {
   const noRenderCondition = condition1 || condition2;
 
   const useStore = useUploadUpdateStore(isUpdate);
-  const states = useStore((state) => state);
-  const { basicInfo, validation } = states;
-  const [gender, main, sub] = basicInfo.category;
+  const [gender, main, sub] = useStore((state) => state.basicInfo.category);
   const breadCrumb = getBreadcrumb(categoryData, sub || main || gender) || '';
 
-  const mainCategory = useMemo(
-    () => getJudgeCategory(breadCrumb),
-    [breadCrumb],
+  const mainCategory = getJudgeCategory(breadCrumb);
+  const styleProps = styleData(styles, colors);
+  const sizeProps = sizeData(
+    mainCategory,
+    sizes as unknown as res.KindStaticData,
   );
-  const styleProps = useMemo(() => styleData(styles, colors), [styles, colors]);
-  const sizeProps = useMemo(
-    () => sizeData(mainCategory, sizes as unknown as res.KindStaticData),
-    [sizes, mainCategory],
-  );
-  const reviewDatas = useMemo(
-    () => ({ pollution: pol, lengths, bodyShapes, fits }),
-    [bodyShapes, fits, lengths, pol],
-  );
-  const review = useMemo(
-    () => reviewData(mainCategory, reviewDatas),
-    [mainCategory, reviewDatas],
-  );
+  const reviewDatas = { pollution: pol, lengths, bodyShapes, fits };
+  const review = reviewData(mainCategory, reviewDatas);
   // TODO: 사이즈, 리뷰 데이터 state 초기화
   // TODO: 서버에서 받은 height, bodyShape 상태 저장하기
-
-  const isFormValid = Object.values(validation).every((x) => x);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (isFormValid) {
-      const body = uploadStateToData(states);
-      if (isUpdate) updateMutate({ id, body });
-      else mutate(body);
-    } else {
-      toastError({ message: '필수 정보를 알려주세요.' });
-    }
-  };
 
   if (!isMounted || noRenderCondition) return null;
   return (
@@ -100,7 +65,7 @@ function UploadTemplate({ id, isUpdate }: Props) {
         title={`상품${isUpdate ? '수정' : '등록'}`}
         left={<BackBtn color="#000" className={$.back} />}
       />
-      <form className={$.upload} onSubmit={handleSubmit}>
+      <form className={$.upload}>
         <ImgUpload {...{ categoryData, isUpdate }} />
         <StyleSelect {...{ isUpdate }} data={styleProps} />
         <Contact {...{ isUpdate }} />
@@ -110,14 +75,7 @@ function UploadTemplate({ id, isUpdate }: Props) {
         <SellerReview {...{ isUpdate }} data={review} />
         <MeasureInfo {...{ isUpdate }} mainCategory={mainCategory} />
         <AdditionInfo {...{ isUpdate }} data={additionData} />
-        <ButtonFooter
-          background="white"
-          type="submit"
-          style={{ padding: '0 21px 30px' }}
-          disabled={!isFormValid}
-        >
-          올리기
-        </ButtonFooter>
+        <SubmitBtn {...{ isUpdate, id }} />
       </form>
     </>
   );
